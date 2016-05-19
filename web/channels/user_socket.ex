83c35e2552767d3cc159aac8a3cc6eae7667f3c3
Stuart Coyle <stuart.coyle@gmail.com>
2016-05-19 16:43:41 +1000

    Heroku setup

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule Arithmex.UserSocket do
   # channel "rooms:*", Arithmex.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
